b4f1f9993fc44c77f82a8c52ed9a692719ae6a8e
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-06-18 15:29:05 +0200

    map

--- Usefull/map.py
+++ Usefull/map.py
@@ -9,3 +9,8 @@ print(result)
 # converting map object to set
 numbersSquare = set(result)
 print(numbersSquare)
+
+# The map() function executes a specified function for each item in an iterable.
+# The item is sent to the function as a parameter.
+# ***Syntax
+# map(function, iterables)
