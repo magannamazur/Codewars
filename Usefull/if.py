e28432b1c28f356ed26cdfce72b8c74931afb797
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-06-18 22:01:25 +0200

    if

--- Usefull/if.py
+++ Usefull/if.py
@@ -2,3 +2,6 @@ def hoopCount(n):
     return "Keep at it until you get it" if n<10 else "Great, now move on to tricks"
 
 print(hoopCount(10))
+
+def get_real_floor(n):
+    return n - 2 if n > 13 else n - 1 if n > 0 else n
