8121466f80b100de640315de6fbf624f388a63ad
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-06-19 15:53:01 +0200

    sum up

---
+++ NumPy_SDA/ex_sumup.py
@@ -0,0 +1,11 @@
+# (1)
+#
+#     Stwórz np.ndarray zawierający liczby od 0 do 100.
+#     Stwórz np.ndarray zawierający 70 jednakokowo od siebie odległych liczb z przedziału -5 do 5
+#
+# (2)
+#
+#     Stwórz np.arrayzawierający cosinusy i sinusy wartości bedących w przedziale od 0 do pi, kolejne liczby powinny być od siebie większe o 1/10 radiana.
+#     Stwórz np.array zawierający liczby od -100 do 100, gdzie każda następna liczba jest większa od poprzedniej o 2 (-100, -98, -96… 98, 100).
+#     Oblicz 20 pierwszych kwadratów.
+#     Oblicz sumę tablic, tak aby element na pozycji ‘i’ był sumą i-tego elementu z pierwszej tablicy oraz elementu len(array)-1-i drugiej tablicy. Użyj tablicy pierwszych 20 kwadratów oraz 20 sześcianów.
