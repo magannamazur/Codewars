d0e1ed11ae52d79484b63bf6d18d3c5651be9120
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-06-22 11:37:18 +0200

    set

---
+++ Usefull/set.py
@@ -0,0 +1,10 @@
+# Set Items
+#
+# Set items are unordered, unchangeable, and do not allow duplicate values.
+
+thisset = ("apple", "banana", "cherry", "apple")
+bybrackets ={"apple", "banana", "cherry", "apple"}
+setted = set(thisset)
+print(thisset)
+print(bybrackets)
+print(setted)
