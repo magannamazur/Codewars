83238222066067f49d0cb7bceb42854f6cfb61a7
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-16 17:01:58 +0200

    Find the odd int

---
+++ Codewars/ex17.py
@@ -0,0 +1,25 @@
+# Find the odd int
+#
+# Given an array of integers, find the one that appears an odd number of times.
+#
+# There will always be only one integer that appears an odd number of times.
+# Examples
+#
+# [7] should return 7, because it occurs 1 time (which is odd).
+# [0] should return 0, because it occurs 1 time (which is odd).
+# [1,1,2] should return 2, because it occurs 1 time (which is odd).
+# [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+# [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+def find_it(seq):
+    for e in seq:
+        x = seq.count(e)
+        if x % 2 == 1:
+            return e
+
+import unittest
+class testex16(unittest.TestCase):
+    def test_ex16(self):
+            self.assertEqual(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+            self.assertEqual(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1)
+            self.assertEqual(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5)
