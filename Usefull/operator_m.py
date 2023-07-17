0fc49bc1a19c81872dd1f2d97f143fd227380786
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-17 12:47:45 +0200

    operator

--- Usefull/operator_m.py
+++ Usefull/operator_m.py
@@ -23,3 +23,15 @@ print(functools.reduce(operator.mul, lis))
 # using reduce to concatenate string
 print("The concatenated product is : ", end="")
 print(functools.reduce(operator.add, ["geeks", "for", "geeks"]))
+
+
+# A list like [1, 2, 3, 1, 2, 3, 4] can be thought of as [1, 1, 2, 2, 3, 3, 4].
+# 1 xor 1 = 0
+# 0 xor 2 = 2
+# 2 xor 2 = 0
+import operator
+from functools import reduce
+
+def find_itgenius(xs):
+     return reduce(operator.xor, xs)
+print(find_itgenius([1,1,2,2,3,3,4]))
