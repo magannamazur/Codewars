89c2d2069b4310c74c4bf01654a7cd86c5a7d732
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-17 12:45:37 +0200

    Find the odd int

--- Codewars/ex17.py
+++ Codewars/ex17.py
@@ -14,8 +14,22 @@
 def find_it(seq):
     for e in seq:
         x = seq.count(e)
-        if x % 2 == 1:
+        if x % 2:
             return e
+
+def find_itfast(seq):
+    return [e for e in seq if seq.count(e) % 2][0]
+#The [0] at the end of the list comprehension selects the first element of the filtered list
+
+import operator
+from functools import reduce
+def find_itgenius(xs):
+     return reduce(operator.xor, xs)
+
+# A list like [1, 2, 3, 1, 2, 3, 4] can be thought of as [1, 1, 2, 2, 3, 3, 4].
+# 1 xor 1 = 0
+# 0 xor 2 = 2
+# 2 xor 2 = 0
 
 import unittest
 class testex16(unittest.TestCase):
