fc734cbcad4cb382352ebfa5dedbf4a54e911d86
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-19 14:00:17 +0200

    Find the unique number

--- Codewars/ex18.py
+++ Codewars/ex18.py
@@ -1,4 +1,12 @@
-
+# Find the unique number
+# There is an array with some numbers. All numbers are equal except for one. Try to find it!
+#
+# find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+# find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+#
+# It’s guaranteed that array contains at least 3 numbers.
+#
+# The tests contain some very huge arrays, so think about performance.
 
 def find_uniq(arr):
     l = set(arr)
@@ -7,6 +15,19 @@ def find_uniq(arr):
         x = arr.count(e)
         if x == 1:
             return e
+
+def find_uniqfast(arr):
+    a, b = set(arr)
+    return a if arr.count(a) == 1 else b
+
+def find_uniqintrested(arr):
+    a = sorted(arr)
+    return a[0] if a[0] != a[1] else a[-1]
+
+from collections import Counter
+
+def find_uniqcounter(a):
+    return Counter(a).most_common()[-1][0]
 
 import unittest
 class testex16(unittest.TestCase):
