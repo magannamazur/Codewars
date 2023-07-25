44acc8d412d3f4f3509df6ecf5210dd96a7b7d56
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-25 21:54:29 +0200

    sort

---
+++ Usefull/sort_sorted.py
@@ -0,0 +1,11 @@
+# Definition and Usage
+#
+# The sorted() function returns a sorted list of the specified iterable object.
+
+a = ("b", "g", "a", "d", "f", "c", "h", "e")
+x = sorted(a)
+print(x)
+
+b = ("hello world")
+x = sorted(b)
+print(x)
