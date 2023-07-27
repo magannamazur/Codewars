ce5a4f74788263fd15b2cf500f341dd4cc486056
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-27 11:02:16 +0200

    Mumbling

---
+++ Codewars/ex19.py
@@ -0,0 +1,24 @@
+# Mumbling
+#
+# This time no story, no theory. The examples below show you how to write function accum:
+# Examples:
+#
+# accum("abcd") -> "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+def accum(s):
+    num = 1
+    st = []
+    for e in s:
+        h = (e*num).capitalize()
+        st.append(h)
+        num+=1
+    return "-".join(st)
+
+import unittest
+class testex18(unittest.TestCase):
+    def test_ex18(self):
+            self.assertEqual(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+            self.assertEqual(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+            self.assertEqual(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
