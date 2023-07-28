c2212cece138eab5b436129bed26df41e4d68b59
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-28 12:54:13 +0200

    Center

---
+++ Usefull/center.py
@@ -0,0 +1,9 @@
+# Definition and Usage
+#
+# The center() method will center align the string, using a specified character (space is default) as the fill character.
+
+# Print the word "banana", taking up the space of 20 characters, with "banana" in the middle:
+txt = "banana"
+x = txt.center(20)
+print(x)
+# '       banana       '
