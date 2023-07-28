e4492966d9ea1a82240c6a37f39e8611a1b8b97c
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-28 12:54:03 +0200

    Build Tower

--- Codewars/ex20.py
+++ Codewars/ex20.py
@@ -13,3 +13,24 @@
 #   " ********* ",
 #   "***********"
 # ]
+
+def tower_builder(n_floors):
+    lvl = ' '
+    s = "*"
+    leaves = lvl*(n_floors-1) + s + lvl*(n_floors-1)
+    tree = [leaves]
+    if n_floors > 1:
+        for e in range(1,n_floors):
+            leaves = lvl*(n_floors - e-1) + s* (2*e +1) + lvl*(n_floors - e-1)
+            tree.append(leaves)
+    return tree
+
+def tower_builderfast(n):
+    return [("*" * (i*2-1)).center(n*2-1) for i in range(1, n+1)]
+
+import unittest
+class testex20(unittest.TestCase):
+    def test_ex20(self):
+            self.assertEqual(tower_builder(1), ['*', ])
+            self.assertEqual(tower_builder(2), [' * ', '***'])
+            self.assertEqual(tower_builder(3), ['  *  ', ' *** ', '*****'])
