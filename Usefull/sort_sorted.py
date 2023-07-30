acba28374600e5923967b63d35bbadc92ded5b61
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-30 14:35:12 +0200

    sort

--- Usefull/sort_sorted.py
+++ Usefull/sort_sorted.py
@@ -9,3 +9,21 @@ print(x)
 b = ("hello world")
 x = sorted(b)
 print(x)
+
+# sorted() method sorts the given sequence as well as set and dictionary(which is not a sequence) either in ascending
+# order or in descending order(does unicode comparison for string char by char) and always returns the a sorted list.
+# This method doesn’t effect the original sequence
+# List
+x = ['q', 'w', 'r', 'e', 't', 'y']
+print(sorted(x))
+print(x)
+
+# sort() function is very similar to sorted() but unlike sorted it returns nothing and makes changes to the original
+# sequence. Moreover, sort() is a method of list class and can only be used with lists.
+# List of Integers
+numbers = [1, 3, 4, 2]
+
+# Sorting list of Integers
+numbers.sort()
+
+print(numbers)
