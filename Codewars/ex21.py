2276d7e8bc2bc9b45114e2bfc561f7caa3c99041
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-31 16:04:14 +0200

    Are they the "same"?

--- Codewars/ex21.py
+++ Codewars/ex21.py
@@ -27,7 +27,7 @@ class testex21(unittest.TestCase):
             self.assertEqual(comp(a1, a2), True)
             a1 = [121, 144, 19, 161, 19, 144, 19, 11]
             a2 = [11 * 21, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
-            self.assertEqual(comp(a1, a2, False)
+            self.assertEqual(comp(a1, a2), False)
             a1 = [121, 144, 19, 161, 19, 144, 19, 11]
             a2 = [11 * 11, 121 * 121, 144 * 144, 190 * 190, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
             self.assertEqual(comp(a1, a2), False)
