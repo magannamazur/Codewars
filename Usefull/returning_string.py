cbecb9aff98c988e01c3753554513b3113cf3f03
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-07-31 17:05:25 +0200

    returning string

---
+++ Usefull/returning_string.py
@@ -0,0 +1,8 @@
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
+
+def greet(name):
+    return "Hello, {} how are you doing today?".format(name)
+
+def greet(name):
+    return "Hello, " + name + " how are you doing today?"
