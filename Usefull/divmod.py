a965e0536352603236ef038d2a72aead4bb6a745
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-08-25 11:13:05 +0200

    divomod

---
+++ Usefull/divmod.py
@@ -0,0 +1,8 @@
+# The divmod() method takes two numbers as arguments and returns their quotient and remainder in a tuple.
+
+# returns the quotient and remainder of 8/3
+result = divmod(8, 3)
+
+print('Quotient and Remainder = ',result)
+
+# Output: Quotient and Remainder =  (2, 2)
