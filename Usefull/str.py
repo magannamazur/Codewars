22a70c16344eff0ea5c9f7ae2b1ddac37f965956
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-08-29 10:12:16 +0200

    str

--- Usefull/str.py
+++ Usefull/str.py
@@ -2,3 +2,18 @@
 float = 2.154327
 format_float = "{:.2f}".format(float)
 print(format_float)
+
+## declaring variables
+name = "Datacamp"
+type_of_company = "Educational"
+
+## enclose your variable within the {} to display it's value in the output
+print(f"{name} is an {type_of_company} company.")
+
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return "%s likes this" % names[0]
+    elif len(names) == 2:
+        return "%s and %s like this" % (names[0], names[1])
