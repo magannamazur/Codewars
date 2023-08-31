34209ad12a983d5d2899fd4a55d7d2d750dadcd5
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-08-31 21:42:10 +0200

    reggex

---
+++ Usefull/regex.py
@@ -0,0 +1,18 @@
+import re
+
+# A sample text string where regular expression
+# is searched.
+string = """Hello my Number is 123456789 and 
+             my friend's number is 987654321"""
+
+# A sample regular expression to find digits.
+regex = '\d+'
+
+match = re.findall(regex, string)
+print(match)
+
+
+
+from re import findall
+def count_smileys(arr):
+    return sum(1 for _ in findall(r"[:;][-~]?[)D]", " ".join(arr)))
