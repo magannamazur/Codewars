7a26b0a3f0b8cd49ead42be9d5677ceb18976429
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-09-01 17:43:01 +0200

    if

--- Usefull/if.py
+++ Usefull/if.py
@@ -11,3 +11,13 @@ def positive_sum(arr):
 
 def update_light(current):
     return 'yellow' if current =='green' else  'red' if current =='yellow'  else 'green'
+
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
