03fcc4debf6b0e9af6809691cc00fa9781be07c5
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-09-13 22:27:56 +0200

    enumerate

--- Usefull/enumerate.py
+++ Usefull/enumerate.py
@@ -10,3 +10,13 @@ print(enumerate_prime)
 
 # convert enumerate object to list
 print(list(enumerate_prime))
+
+def number(lines):
+  return ['%d: %s' % v for v in enumerate(lines, 1)]
+
+# % is the old style Python string formatting operator. On the left side is a string template and on the right side
+# is either a single value or a tuple that supplies the values to fill in the template.
+#
+# In this case, the enumerate function gives us a list of tuples [(1, line1), (2, line2), ...]
+#
+# Then the list comprehension invokes the string formatter on each tuple, so we get ["1: line1", "2: line2"...]
